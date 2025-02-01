2c1099827689e6278cc339d2a6b17e8f62909c3d
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2025-02-01 19:49:38 +0600

    Api Mocking and Api Response Modification

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://angular-ngrx-nx-realworld-example-app-lyart.vercel.app/home');
@@ -8,25 +9,47 @@ test.beforeEach(async ({ page }) => {
   await page.getByPlaceholder('Password').fill('1q1q1q1q');
   await page.locator('[data-e2e-id="sign-in"]', { hasText: 'Sign in' }).click();
 
-  // Wait for successful login - Ensure it navigates or an element appears
-  await page.waitForURL('**/home'); // Adjust based on actual navigation behavior
-
+  // Wait for successful login
+  await page.waitForURL('**/home');
+  //Lecture 3 : Answers////////////////////////////////////////
   // **Mock API AFTER login but BEFORE it's called**
-  await page.route('https://real-world-app-39656dff2ddc.herokuapp.com/api/tags', async route => {
-    const tags = {
-      "tags": ["test", "automations", "playwright"]
-    };
+  // await page.route('*/**/api/tags', async route => {
+  //   await route.fulfill({
+  //     body: JSON.stringify(tags),
+  //     contentType: 'application/json',
+  //   });
+  // });
+
+  // // **Force the app to re-fetch the API (if needed)**
+  // await page.reload({ waitUntil: 'networkidle' }); 
+  // // Ensures fresh requests
+
+  //////////////////////////////////////////////////////////////
+  //Lecture 4 : Answers////////////////////////////////////////
+  // Mock API response
+  await page.route('*/**/api/articles*', async route => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = 'New Title';
+    responseBody.articles[0].description = 'New Descriptionowpwpwpw';
+    responseBody.articles[0].body = 'New Body';
+
     await route.fulfill({
-      body: JSON.stringify(tags),
-      contentType: 'application/json',
+      body: JSON.stringify(responseBody),
+      contentType: 'application/json'
     });
   });
-
-  // **Force the app to re-fetch the API (if needed)**
-  await page.reload({ waitUntil: 'networkidle' }); // Ensures fresh requests
+  //////////////////////////////////////////////////////////////
 });
 
 test('has title', async ({ page }) => {
   const title = await page.locator('.navbar-brand').textContent();
   await expect(title).toBe('conduit');
+  await page.close();
+});
+
+test('apiResponseCheck', async ({ page }) => {
+  await page.locator('.feed-toggle').getByText('Global Feed').click();
+  await expect.soft(await page.locator('[data-e2e-id="article-list"]').nth(0).locator('p')).toHaveText('New Descriptionowpwpwpw');
+  await expect.soft(await page.locator('[data-e2e-id="article-list"]').nth(0).locator('h1')).toHaveText('New Title');
 });
